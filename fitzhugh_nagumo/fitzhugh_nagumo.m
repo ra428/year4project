006aaef82e7b61855fb752915fcd949f571b3ce6
R.A. Kurien <ra428@tw408.eng.cam.ac.uk>
2015-11-04 20:26:54 +0000

    It works!

--- fitzhugh_nagumo/fitzhugh_nagumo.m
+++ fitzhugh_nagumo/fitzhugh_nagumo.m
@@ -7,15 +7,15 @@ I_app = 0.5;        % External applied current
 epsilon = 0.01;     % Controls how 'fast' v is compared to w
 gamma = 0.5;        % Parameter in equation (5.39)
 dvdt = zeros(2,1);  % empty column
-dvdt(1) = (NL_resistor(v(1)) - v(2))/epsilon; % Equation (5.38)
+dvdt(1) = (NL_resistor(v(1)) - v(2) + I_app)/epsilon; % Equation (5.38)
 dvdt(2) = v(1) - gamma*v(2);        % Equation (5.39)
 
-    function F = NL_resistor(V,varargin)
-        % function F = NL_Resistor(V)
+    function F = NL_resistor(v)
+        % function F = NL_Resistor(v)
         % Model the nonlinear resistor with input V and alpha (optional)
         % Equation (5.40) in Keener and Sneyd book
         alpha = 0.1;            % Control the shape of F(V)
-        F = V*(1-V)*(V-alpha);
+        F = v*(1-v)*(v-alpha);
     end
 end
 
