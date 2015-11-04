54d551de92d83cc8dd9649dbfcb06b5915698c9d
R.A. Kurien <ra428@tw408.eng.cam.ac.uk>
2015-11-04 18:09:41 +0000

    More work

--- fitzhugh_nagumo/fitzhugh_nagumo.m
+++ fitzhugh_nagumo/fitzhugh_nagumo.m
@@ -1,24 +1,21 @@
+function dvdt = fitzhugh_nagumo(t,v)
+% function dvdt = fitzhugh_nagumo(t,v)
 % FitzHugh-Nagumo model
+% From Keener and Sneyd book, equations (5.38), (5.39). (5.40)
 
+I_app = 0.5;        % External applied current                
+epsilon = 0.01;     % Controls how 'fast' v is compared to w
+gamma = 0.5;        % Parameter in equation (5.39)
+dvdt = zeros(2,1);  % empty column
+dvdt(1) = (NL_resistor(v(1)) - v(2))/epsilon; % Equation (5.38)
+dvdt(2) = v(1) - gamma*v(2);        % Equation (5.39)
 
-
-    tspan = [0 100]; % Time interval
-    v0 = 0.5 ; % Initial condition
-    
-    [t,v ] = ode45(@dstate, tspan, v0);
-    plot(t,y)
-    function dstate(t,v)
-    
-        function F = NL_Resistor(V,varargin)
-        % function F = NL_Resistor(V,varargin)
+    function F = NL_resistor(V,varargin)
+        % function F = NL_Resistor(V)
         % Model the nonlinear resistor with input V and alpha (optional)
-            if size(varargin,1)<0
-                alpha = 0.1;
-            else
-                alpha = varargin(1);
-            end
-
-            F = V*(1-V)*(V-alpha);
-        end
-
+        % Equation (5.40) in Keener and Sneyd book
+        alpha = 0.1;            % Control the shape of F(V)
+        F = V*(1-V)*(V-alpha);
+    end
 end
+
