790f81f034b55e0c94e187a6d9db793e20a5ac6d
ra428 <rajiv5ashok@gmail.com>
2015-11-09 12:51:26 +0000

    Implemented relay approximation of FitzHugh-Nagumo Need to now test sensitiviy of relay characterisitcs  in order to more closely match the hysteresis of the actual system

--- fitzhugh_nagumo/test_fitz_nagumo.m
+++ fitzhugh_nagumo/test_fitz_nagumo.m
@@ -1,15 +1,27 @@
 % Test out FitzHugh-Nagumo model from Keener and Sneyd Chapter 5
 clear all
 close all
-tmax = 20;
+tmax = 3;
 % Set up various variables
 tspan = [0 tmax];      % Time interval
-v0 = [0.5;0.5];     % Intial condition
+v0 = [0.9864;0.5];     % Intial condition [voltage current]
 
 % ODE solver
 %global f_relay
 [t,v] = ode45(@fitzhugh_nagumo, tspan, v0);
-%[t,v] = ode45(@fn_relay, tspan, v0);
+
+% Variables for simulink
+T = 0.9104; tau = 0.4496;
+d1 = 0.9864;
+d2 = 0.2829;
+e = getAsymmetricRelayHysteresisForFitzNagumo(T,tau,d1,d2);
+epsilon_1 = e(1);
+epsilon_2 = e(2);
+
+% Simulink
+set_param('fitz_relay/Relay','OffOutputValue','d1','OnOutputValue','-d2','OffSwitchValue','epsilon_1', 'OnSwitchValue','epsilon_2' )
+simulate_fitz = sim('fitz_relay') % Returns  a variable called 'simout'
+
 
 
 %% Show Results
@@ -17,6 +29,7 @@ v0 = [0.5;0.5];     % Intial condition
 figure(1)
 plot(t,v(:,1))
 hold on
+plot(simout.time,simout.data(:,1))
 %plot(linspace(0,tmax,size(f_relay,2)),f_relay,'ro')
 xlabel('Time')
 ylabel('Voltage')
@@ -29,7 +42,8 @@ hold on
 v2 = linspace(min(v(:,1))-0.1, max(v(:,1))+0.1, 100);
 alpha = 0.1; I_app = 0.5;
 f = (-v2.^3 + (1+alpha).*v2.^2 - alpha.*v2)+I_app;
-plot(f,v2,'--')
+%plot(f,v2,'--')
+plot(simout.data(:,2),simout.data(:,1))
 xlabel('Current')
 ylabel('Voltage')
 
