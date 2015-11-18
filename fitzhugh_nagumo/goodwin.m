03782e5a595909b76fec6a5441ad42af0bb972f6
R.A. Kurien <ra428@tw908.eng.cam.ac.uk>
2015-11-18 12:33:49 +0000

    goodwin relay approximation working

--- fitzhugh_nagumo/goodwin.m
+++ fitzhugh_nagumo/goodwin.m
@@ -6,14 +6,12 @@ function dxdt = goodwin(t,x)
 global p
 global b
 
-
-
-
 dxdt = zeros(3,1);  % empty column
 % Differential equations
 dxdt(1) = -b*x(1) + 1/(1+x(3)^p);   
 dxdt(2) = b*(x(1)-x(2));
 dxdt(3) = b*(x(2)-x(3));
+
 end
 
 
