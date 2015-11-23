19524c3a80d1537470cf141c666e5c03e60d2ae3
R.A. Kurien <ra428@tw415.eng.cam.ac.uk>
2015-11-23 16:53:54 +0000

    new fitz relay

--- fitzhugh_nagumo/test_fitz_nagumo.m
+++ fitzhugh_nagumo/test_fitz_nagumo.m
@@ -11,12 +11,13 @@ v0 = [0.9;0.5];        % Intial condition [voltage current]
 [t,v] = ode15s(@fitzhugh_nagumo, tspan, v0);
 
 %% Variables for simulink
-                            epsilon_1 = min (v(:,2));   %0.4723;
-                            epsilon_2 = max(v(:,2));    %0.6666;
+I_app = 0.5;
+                            epsilon_1 = min(v(:,2)) - I_app;   %0.4976;
+                            epsilon_2 = max(v(:,2)) - I_app;    %0.6262;
                             %                             %T = 0.9104; tau = 0.4496;
 T = 0.519; tau = 0.1975;
                             %d1 = 0.9864; d2 =0.2829; 
-d1 = 0.75;d2 = 0.25;
+d1 = 0.01;d2 = 1;
 gamma = 0.5;
 alpha = -2;
                             %A = zeros_of_theorem_5_1(epsilon_1, epsilon_2, d1, d2, T, tau);
@@ -27,7 +28,7 @@ B = 1; C = 1; D = 0;
 % epsilon_2 = out(2)
 [a1 , a2 ] = getInitialConditionsAsymmRelay(A,B,C,d1,d2,T,tau);
                             %X0 = 0.5;
-X0 =a1;
+X0 =a2;
 stable = checkStability(A,B,C,T,tau,d1,d2) % Stability check
 
 
@@ -42,7 +43,7 @@ simulate_fitz = sim('fitz_relay');
 figure(1)
 plot(t,v(:,1))
 hold on
-plot(relay_output.time,relay_output.data(:,1)+ alpha*current.data(:,1))
+plot(relay_output.time,relay_output.data(:,1)+ alpha*(current.data(:,1) -I_app) )
 %plot(simout.time,relay_output.data(:,1))
 xlabel('Time')
 ylabel('Voltage')
@@ -65,15 +66,15 @@ hold off
 
 % limit cycle
 figure(2)
-plot(v(:,1),v(:,2))
+plot(v(:,2),v(:,1))
 hold on
 v2 = linspace(min(v(:,1))-0.1, max(v(:,1))+0.1, 100);
-alpha_cubic = 0.1; I_app = 0.5;
+alpha_cubic = 0.1; 
 f = (-v2.^3 + (1+alpha_cubic).*v2.^2 - alpha_cubic.*v2)+I_app;
-plot(v2,f,'--')
-plot((relay_output.data(:,1) + alpha*(current.data(:,1))),current.data(:,1))
-ylabel('Current')
-xlabel('Voltage')
+plot(f,v2,'--')
+plot(current.data(:,1)+I_app,(relay_output.data(:,1) + alpha*(current.data(:,1)-I_app)))
+xlabel('Current')
+ylabel('Voltage')
 % axis([0.35 0.8 -0.4 1])
 
 
