d8e20b80e717d463fe0de6053dbe2816ef86e502
R.A. Kurien <ra428@tw906.eng.cam.ac.uk>
2016-01-29 16:47:08 +0000

    switches states, but doenst oscillate

--- bursting/fast_nullcline_winged_cusp.m
+++ bursting/fast_nullcline_winged_cusp.m
@@ -1,16 +1,32 @@
+clear
+
+
 bump = @(x,delta) (tanh(x+delta) - tanh(x-delta) -2*tanh(delta));
 fast_nullcline = @(xs,xf,u,gamma,beta,alpha,delta,ef)...
     ((-xf + tanh(xf+ bump(u + xs +0.5*gamma*xf,delta) + beta*xf+ alpha))/ef);
-linear_plant = @(xs,xf) (xf-.35*xs);
+linear_plant = @(xs,xf,ts) (xf-ts*xs);
                     
-% alpha = 0.4;
-alpha =.5;
-u = 1.25 ; beta = 0.5;
+% alpha = .2;
+alpha = 0.25;
+beta = 0.5;
 gamma = 1;
-ef = 0.0075;
-delta = .5;
+delta = 0.5;
+u = 0.5 ;
+tf = 0.0075;
+ts = 1;
+
+% % alpha = 0.7;
+% % beta = 0.5-1;
+% % gamma = 0.3/2;
+% % delta = 1;
+% % u = 0.7;
+% tau_f = 0.0075;
+% tau_s = 1;
+% xf_0 = -1;
+% xs_0 = -1;
 
 cc = hsv(15);
+
 % for iteration = 1:5
 %     alpha = iteration/5;
 % 
@@ -41,23 +57,20 @@ cc = hsv(15);
 % end
 % legend('\gamma = 0.','\gamma = 0.2','\gamma = 0.4','\gamma = 0.6','\gamma = 0.8')
 
-% for iteration = 1:5
-%     ef = .0075/(5*iteration);
 
-    h1 = ezplot(@(x,y)fast_nullcline(x,y,u,gamma,beta,alpha,delta,ef),[-4,4])
-    set(h1,'Color',cc(10,:));
-    grid on
-    hold on
-    
-% end
-% legend('\delta = 0.0','\delta = 0.2','\delta = 0.4','\delta = 0.6','\delta = 0.8')
+% Plot winged cusp
+h1 = ezplot(@(x,y)fast_nullcline(x,y,u,gamma,beta,alpha,delta,tf),[-4,4]);
+set(h1,'Color',cc(floor(15*rand(1)),:));
+hold on
 
-h2 = ezplot(@(x,y)linear_plant(x,y),[-4,4])
+% Plot linear plant
+h2 = ezplot(@(x,y)linear_plant(x,y,ts),[-4,4]);
+
 title('Phase portrait')
-[x,y] = meshgrid(-4:0.1:4,-4:0.1:4);
+[x,y] = meshgrid(-4:1:4,-4:1:4);
+grid on
 
-xf_dot = fast_nullcline(x,y,u,gamma,beta,alpha,delta,ef);
-xs_dot = linear_plant(x,y);
-%quiver(x,y,xs_dot,xf_dot)
-% myfunc = @(x,y,k) (x.^k -y.^k -1);
-% ezplot(@(x,y)myfunc(x,y,2))
+% Plot trajectories
+xf_dot = fast_nullcline(x,y,u,gamma,beta,alpha,delta,tf);
+xs_dot = linear_plant(x,y,ts);
+quiver(x,y,xs_dot,xf_dot)
